e045443a848f2d087afada7ea8a3fdf58a4d7122
Kubsa Melkamu <kubsamlkm@gmail.com>
2025-03-25 22:17:25 +0300

    middleware:for admin user and authorized user implemented

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+
+export const protect = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized, no token provided" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Invalid or Token Expired" });
+  }
+};
+
+export const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {  
+    next();
+  } else {
+    res.status(403).json({ message: "Access denied. Admins only!" });
+  }
+};
