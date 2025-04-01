9e095107ffad1e866c3659fba3d7015004bf550f
Kubsa Melkamu <kubsamlkm@gmail.com>
2025-04-01 12:00:58 +0300

    review & rating functionailty : implemented

--- backend/models/productmodel.js
+++ backend/models/productmodel.js
@@ -1,5 +1,19 @@
 import mongoose from "mongoose";
 
+const reviewSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
 const productSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
@@ -10,6 +24,9 @@ const productSchema = new mongoose.Schema(
     countInStock: { type: Number, required: true, default: 0 },
     image: { type: String, required: true },
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    reviews: [reviewSchema],
+    numReviews: { type: Number, required: true, default: 0 },
+    rating: { type: Number, required: true, default: 0 },
   },
   { timestamps: true }
 );
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -86,5 +86,68 @@ router.delete("/:id", protect, admin, async (req, res) => {
   }
 });
 
+router.post("/:id/reviews", protect, async (req, res) => {
+
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    
+    const { rating, comment } = req.body;
+
+    const alreadyReviewed = product.reviews.find(
+      (rev) => rev.user.toString() === req.user._id.toString()
+    );
+    if (alreadyReviewed) {
+      return res.status(400).json({ message: "Product already reviewed" });
+    }
+
+    const review = {
+      user: req.user._id,
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+    };
+
+    product.reviews.push(review);
+    product.numReviews = product.reviews.length;
+
+    product.rating =
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.reviews.length;
+
+    await product.save();
+    res.status(201).json({ message: "Review added", reviews: product.reviews });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+router.delete("/:productId/reviews/:reviewId", protect,admin, async (req, res) => {
+
+  try {
+    const product = await Product.findById(req.params.productId);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    product.reviews = product.reviews.filter(
+      (rev) => rev._id.toString() !== req.params.reviewId
+    );
+    product.numReviews = product.reviews.length;
+    product.rating =
+      product.reviews.length === 0
+        ? 0
+        : product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+          product.reviews.length;
+
+    await product.save();
+    res.json({ message: "Review removed", reviews: product.reviews });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
 
 export default router;
