2fa7c136cb58cbb6ecda07d6098af81d467d7cdf
Kubsa Melkamu <kubsamlkm@gmail.com>
2025-04-05 16:11:54 +0300

    wrap App component : persistGate

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,15 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
-import store from "../store/store";
-import Layout from "@/components/layout/Layout";
+import store, { persistor } from "../store/store";
+import { PersistGate } from "redux-persist/integration/react";
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Provider>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Component {...pageProps} />
+        </PersistGate>
+      </Provider>
   );
 }
+
+
