c88580eb5e9ec5405f3f2b8706583b99c0d4204b
Kubsa Melkamu <kubsamlkm@gmail.com>
2025-04-05 16:10:13 +0300

    update store : to persist user status with redux-persist

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,21 +1,53 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice'; 
 import cartReducer from './slices/cartSlice';
 import orderReducer from './slices/orderSlice';
 import productReducer from './slices/productSlice';
 import wishlistReducer from './slices/wishListSlice';
 
-const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    cart: cartReducer,
-    orders: orderReducer,
-    products: productReducer,
-    wishlist: wishlistReducer,
-  },
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  cart: cartReducer,
+  orders: orderReducer,
+  products: productReducer,
+  wishlist: wishlistReducer,
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['auth'], 
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
 
 export default store;
