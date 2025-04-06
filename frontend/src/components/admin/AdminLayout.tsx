1439b362181541913584474c6d3c763112763ba1
Kubsa Melkamu <kubsamlkm@gmail.com>
2025-04-06 22:51:58 +0300

    updated AdminLayout:its layout

--- frontend/src/components/admin/AdminLayout.tsx
+++ frontend/src/components/admin/AdminLayout.tsx
@@ -15,23 +15,25 @@ const AdminLayout = ({ children }: AdminLayoutProps) => {
 
   return (
     <div className="min-h-screen flex">
-      <div className="hidden md:block">
+      <aside className="hidden md:block fixed top-0 left-0 w-64 h-screen bg-blue-700 z-30">
         <Sidebar />
-      </div>
+      </aside>
       {sidebarOpen && (
-        <div className="fixed inset-0 z-40 md:hidden flex">
+        <div className="fixed inset-0 z-40 flex md:hidden">
           <div
             className="fixed inset-0 bg-black opacity-50"
             onClick={() => setSidebarOpen(false)}
           ></div>
-          <div className="relative z-50">
+          <div className="relative z-50 w-64 bg-blue-700">
             <Sidebar />
           </div>
         </div>
       )}
-      <div className="flex flex-col flex-grow">
-        <Header onToggleSidebar={toggleSidebar} />
-        <main className="p-6 bg-gray-100 flex-grow">
+      <div className="flex flex-col flex-grow md:ml-64 w-full">
+        <div className="sticky top-0 z-20 bg-white shadow">
+          <Header onToggleSidebar={toggleSidebar} />
+        </div>
+        <main className="flex-grow overflow-auto p-6 bg-gray-100">
           {children}
         </main>
       </div>
