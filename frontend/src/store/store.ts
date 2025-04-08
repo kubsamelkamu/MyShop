bea12d0f4a164b6f9100c0ecfae4b01284e2135b
Kubsa Melkamu <kubsamlkm@gmail.com>
2025-04-08 14:54:44 +0300

    add list of slices to whitelist

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -30,7 +30,7 @@ const rootReducer = combineReducers({
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['auth'], 
+  whitelist: ['auth','cart','orders','products','users','wishlist'], 
 };
 
 
