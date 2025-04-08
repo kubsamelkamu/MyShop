881f5dbc18d694ba3af4c48f8e533d69c2c36662
Kubsa Melkamu <kubsamlkm@gmail.com>
2025-04-08 23:47:36 +0300

    add update endpoing to userRoute:to revoke|make user admin

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { protect, admin } from "../middleware/authMiddleware.js"; 
+import { protect, admin } from "../middleware/authMiddleware.js";
 import User from "../models/UserModel.js";
 
 const router = express.Router();
@@ -10,6 +10,30 @@ router.get("/", protect, admin, async (req, res) => {
     res.json(users);
   } catch (error) {
     res.status(500).json({ message: "Server Error" });
+  }
+});
+
+router.put("/:id", protect, admin, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { isAdmin } = req.body; 
+
+    if (!id || typeof isAdmin === "undefined") {
+      return res.status(400).json({ message: "Missing parameters" });
+    }
+    if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+      return res.status(400).json({ message: "Invalid User ID" });
+    }
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    user.isAdmin = isAdmin;
+    const updatedUser = await user.save();
+    res.json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 });
 
